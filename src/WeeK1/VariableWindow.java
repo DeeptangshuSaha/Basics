b8506b568d64bf87c6f29eea6629271bdeee44d7
DeeptangshuSaha <deeptangshusaha62@gmail.com>
2025-09-05 12:43:05 +0530

    Variable Window Attempted

--- src/WeeK1/VariableWindow.java
+++ src/WeeK1/VariableWindow.java
@@ -1,8 +1,25 @@
 package WeeK1;
 
+import java.util.HashSet;
+
 public class VariableWindow {
     public static void main(String[] args) {
-        
+        //Longest Substring without repeating chars
+        String s = "pwwkew";
+        int left=0, right=0, maxL=0;
+        HashSet<Character> set = new HashSet<Character>();
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if (!set.contains(c)) {
+                set.add(c);
+                maxL = Math.max(maxL, right-left+1);
+                right++;
+            } else {
+                set.remove(s.charAt(left));
+                left++;
+            }
+        }
+        System.out.println(maxL);
     }
 }
 
